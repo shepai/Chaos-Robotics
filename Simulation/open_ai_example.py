816c9e8ac5fa9fd3c2fcc831651df843da6f7743
shepai <dextershepherd@hotmail.co.uk>
2022-10-10 14:18:15 +0100

    one python file example

--- Simulation/open_ai_example.py
+++ Simulation/open_ai_example.py
@@ -5,20 +5,96 @@ from gym import logger as gymlogger
 import matplotlib.pyplot as plt
 from IPython import display as ipythondisplay
 from pyvirtualdisplay import Display
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D 
 import random
-env = gym.make("Acrobot-v1",render_mode="rgb_array")
-observation, info = env.reset()
-show=False
-maxReward=0
-for _ in range(1000):
-    action = env.action_space.sample()
-    action=random.randint(0,2)
-    observation, reward, terminated, truncated, info = env.step(action)
-    rendered=env.render()
-    plt.imshow(rendered)
-    plt.pause(0.05)
-    if terminated or truncated:
-        observation, info = env.reset()
-env.close()
+from copy import deepcopy as dc
 
-print("done")
+SIM='MountainCar-v0'
+
+def run_trial(genotype,num_trials,show=True):
+    env = gym.make(SIM,render_mode="rgb_array")
+    observation, info = env.reset()
+    rewards=0
+    assert len(genotype)>=num_trials, "Make sure the num_trials is not greater than your genotype size"
+    for _ in range(num_trials):
+        action = int(genotype[_])
+        observation, reward, terminated, truncated, info = env.step(action)
+        rewards+=reward
+        
+        if terminated or truncated:
+            observation, info = env.reset()
+    if show:
+        plt.imshow(env.render())
+        plt.show()
+    env.close()
+    return rewards
+
+def visualise(genotype,num_trials):
+    env = gym.make(SIM,render_mode="rgb_array")
+    observation, info = env.reset()
+    rewards=0
+    assert len(genotype)>=num_trials, "Make sure the num_trials is not greater than your genotype size"
+    for _ in range(num_trials):
+        #action = env.action_space.sample()
+        action = int(genotype[_])
+        observation, reward, terminated, truncated, info = env.step(action)
+        rewards+=reward
+        plt.imshow(env.render())
+        plt.pause(0.05)
+        if terminated or truncated:
+            observation, info = env.reset()
+    env.close()
+    return rewards
+
+def lorenz_attr(x, y, z):
+    x_dot = prandtl*(y - x)
+    y_dot = rho*x - y - x*z
+    z_dot = x*y - beta*z
+    return x_dot, y_dot, z_dot
+def get_velocities(prandtl,rho,beta):
+    dt = 0.01
+    num_steps = 10000
+
+    xs = np.empty(num_steps + 1)
+    ys = np.empty(num_steps + 1)
+    zs = np.empty(num_steps + 1)
+    xs[0], ys[0], zs[0] = (0., 1., 1.05)
+
+    for i in range(num_steps):
+        x_dot, y_dot, z_dot = lorenz_attr(xs[i], ys[i], zs[i])
+        xs[i + 1] = xs[i] + (x_dot * dt)
+        ys[i + 1] = ys[i] + (y_dot * dt)
+        zs[i + 1] = zs[i] + (z_dot * dt)
+    return xs,ys,zs
+
+def convert(signal,val=5):
+    #make threshold with val and create a signal based off of the chaotic one
+    new=np.zeros(signal.shape)
+    new[np.argwhere(signal<=(val*-1))]=0
+    new[np.argwhere(signal>(val*-1))]=1
+    new[np.argwhere(signal>val)]=2
+    return new
+
+
+prandtl = 10
+rho = 28
+beta = 8/3 
+
+best=-100
+best_chaotic=None
+for rho in range(28):
+    for prandtl in range(0,10):
+        xs,ys,zs=get_velocities(prandtl,rho,beta)
+        geno=convert(xs,val=5)
+        reward=run_trial(geno,100,show=False)
+        plt.cla()
+        plt.plot(geno[0:100])
+        plt.title("Gen %:"+str((rho+prandtl)/(10*28))+" Reward"+str(reward))
+        plt.pause(0.05)
+        if reward>=best:
+            best=reward
+            best_chaotic=dc(geno) #deep copy the genotype
+print("Reward sum:",best)
+reward=visualise(best_chaotic,100)
