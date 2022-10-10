53eef11f049514a810bb1e13b0326d097fe9c28d
shepai <dextershepherd@hotmail.co.uk>
2022-10-10 13:22:04 +0100

    attempt to use chaos signal in sim

--- Simulation/open_ai_example.py
+++ Simulation/open_ai_example.py
@@ -5,12 +5,14 @@ from gym import logger as gymlogger
 import matplotlib.pyplot as plt
 from IPython import display as ipythondisplay
 from pyvirtualdisplay import Display
-
-env = gym.make("CartPole-v1",render_mode="rgb_array")
+import random
+env = gym.make("Acrobot-v1",render_mode="rgb_array")
 observation, info = env.reset()
-
+show=False
+maxReward=0
 for _ in range(1000):
     action = env.action_space.sample()
+    action=random.randint(0,2)
     observation, reward, terminated, truncated, info = env.step(action)
     rendered=env.render()
     plt.imshow(rendered)
