b6268d37a2309b4835a8df4cda41ad3a7128e24a
shepai <dextershepherd@hotmail.co.uk>
2022-11-09 15:26:46 +0000

    correct maths

--- Simulation/Reward Modelling/reward.py
+++ Simulation/Reward Modelling/reward.py
@@ -48,7 +48,7 @@ def show_gen_control():
         outputs[0][t]=x[0][t+1]
         outputs[1][t]=x[1][t+1]
     
-    return outputs[:,1:]
+    return outputs[:,600:630],x[:,600:630]
 
 #implementing this with random generation
 
@@ -57,7 +57,7 @@ def sigmoid(x):
 
 def step(i,t,mus,Cp,x,j):
     p=pp
-    mus[t+1]=(mus[t]**p)+lambda_*(((x[0][t]-x[0][t-p])**2+(x[1][t]-x[1][t-p])**2)/p)
+    mus[t+1]=(mus[t-1]**p)+lambda_*(((x[0][t-1]-x[0][t-1-p])**2+(x[1][t]-x[1][t-1-p])**2)/p)
     Cp[i][t]=(mus[t])*((weights[i][i]*(x[i][t]-x[i][t-p]))+(weights[i][j]*(x[j][t]-x[j][t-p])))
     a=sigmoid(thetas[i]+(weights[i][i]*x[i][t])+(weights[i][j]*x[j][t])+Cp[i][t])
     x[i][t+1]=a
@@ -69,11 +69,15 @@ def step(i,t,mus,Cp,x,j):
 # Create the figure and the line that we will manipulate
 fig, ax = plt.subplots()
 #fig.subplots_adjust(left=0.05, bottom=0.15, right=0.95, top=0.98, wspace=0.05, hspace=0.05)
-out=show_gen_control()
+out,x=show_gen_control()
 avg_out=((out[0]+out[1])/2)[:-1]
 line, = ax.plot(avg_out, lw=2)
-ax.set_xlabel('Iteration')
+line2, = ax.plot(x[0],c="r",label="neuron 2", lw=2)
+line3, = ax.plot(x[1],c="b",label="neuron 1", lw=2)
+ax.set_xlabel('Iteration +600')
+ax.set_ylabel('Neuron output')
 ax.set_ylim(0,1)
+ax.legend(loc="lower right")
 # adjust the main plot to make room for the sliders
 fig.subplots_adjust(left=0.25, bottom=0.25)
 
@@ -83,7 +87,7 @@ axfreq = fig.add_axes([0.1, 0.1, 0.65, 0.03])
 freq_slider = Slider(
     ax=axfreq,
     label='lambda',
-    valmin=0.1,
+    valmin=0.01,
     valmax=1,
     valinit=lambda_,
 )
@@ -105,9 +109,11 @@ def update(val):
     global pp
     lambda_ = freq_slider.val
     pp=int(amp_slider.val)
-    outputs=show_gen_control()
+    outputs,x=show_gen_control()
     avg_out=((outputs[0]+outputs[1])/2)[:-1]
     line.set_ydata(avg_out)
+    line2.set_ydata(x[0])
+    line3.set_ydata(x[1])
     fig.canvas.draw_idle()
 
 
@@ -118,11 +124,18 @@ amp_slider.on_changed(update)
 # Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
 resetax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
 button = Button(resetax, 'Reset', hovercolor='0.975')
-
+resetax1 = fig.add_axes([0.8, 0.001, 0.1, 0.04])
+button1 = Button(resetax1, 'Reset weights', hovercolor='0.975')
 
 def reset(event):
     freq_slider.reset()
     amp_slider.reset()
-button.on_clicked(reset)
 
+def reset_weight(event):
+    thetas=np.random.random((2,1)) #bias terms
+    weights=np.random.random((2,2))
+    update(event)
+
+button.on_clicked(reset)
+button1.on_clicked(reset_weight)
 plt.show()
