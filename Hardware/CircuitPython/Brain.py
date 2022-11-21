461db3bd13826259955a2482bc6ce43e82fbbece
shepai <dextershepherd@hotmail.co.uk>
2022-11-21 14:37:39 +0000

    behaviours

--- Hardware/CircuitPython/Brain.py
+++ Hardware/CircuitPython/Brain.py
@@ -10,12 +10,15 @@ class Brain:
         self.steps=size
         self.reset()
         self.lambda_=lamda_
+        self.values=[1,4,6,8,9]
         self.p=1 #initial value
         self.sensors=sensor_num
         self.outputs=motor_num
-        self.post_process_bias=np.array([-0.5,-0.6])
-        self.in_nuerons_weights=cp.normal(0,1,size=(sensor_num,2))    
-        self.in_nuerons_biases=cp.normal(0,1,size=(sensor_num,2))/10
+        self.post_process_bias=np.array([0.1,0.1]) #biases to control basic forward motion
+        self.in_nuerons_weights=np.array([[-5.23159,-4.19076],[-5.23159,-5.19295]]) #cp.normal(0,2,size=(sensor_num,2))    
+        self.in_nuerons_biases=np.array([[0.233793,-0.529649],[0.323159,0.218076]]) #cp.normal(-2,2,size=(sensor_num,2))/10
+        self.out_nuerons_weights=np.array([[5.06038, -3.29649],[-5.00933, 5.00933]])#cp.normal(0,2,size=(sensor_num,2))    
+        self.out_nuerons_biases=np.array([[0.122669,0.196101],[0.129649, 0.282701]])#cp.normal(-2,2,size=(sensor_num,2))/10
     def reset(self):
         self.mus=np.zeros((self.steps,))
         self.mus[0]=-1
@@ -25,8 +28,14 @@ class Brain:
         self.x=np.zeros((2,self.steps))
         self.x[0][0]=1#random.randint(0,2)
         self.x[1][0]=1#random.randint(0,2)
-        self.thetas= np.array([[-3.4],[3.8]])#np.random.normal(2,1.5,(2,1)) #bias terms
-        self.weights= np.array([[-22,5.6],[0,-6.6]]) #np.random.normal(2,1.5,(2,2))
+        self.thetas= np.array([[3.4],[3.8]])#np.random.normal(2,1.5,(2,1)) #bias terms
+        self.weights= np.array([[-12,5.6],[0,-6.6]]) #np.random.normal(2,1.5,(2,2))
+        self.t=0
+    def clearOld(self):
+        #reset arrays but keep last item
+        self.mus=np.zeros((self.steps,))+self.mus[-1]
+        self.Cp=np.zeros((2,self.steps))+self.Cp[-1]
+        self.x=np.zeros((2,self.steps))+self.x[-1]
         self.t=0
     def sigmoid(self,x):
         return (1/(1+np.exp(-1*x)))
@@ -39,20 +48,25 @@ class Brain:
         self.x[1][t+1]=self.sigmoid(self.thetas[1]+a[1]+self.Cp[1][t])
     def A(self,inputs):
         #run neural preprocessing on inputs and generate p value
-        out=(self.in_nuerons_weights.transpose()*inputs - self.in_nuerons_biases.transpose()) 
+        out=(self.in_nuerons_weights.transpose()*inputs )- self.in_nuerons_biases.transpose()
         #out[out<0]=0 #make sure non 0 is dealt with
         #assert (self.in_nuerons_weights.transpose()*inputs).shape==(self.in_nuerons_weights.transpose*inputs - self.in_nuerons_biases.transpose()).shape,"Assertion incorrect"
-        return round(np.sum(out))
+        return self.values[round(np.sum(self.sigmoid(out))/4 * len(self.values))] #get selected p value from range 9
+    def B(self,outputs):
+        #run neural post processing to get movement
+        out=(self.out_nuerons_weights.transpose()*outputs )- self.out_nuerons_biases.transpose()
+        return self.sigmoid(np.sum(out,axis=1))
     def run(self,inputs): #take in inputs and produce outputs
         assert len(inputs)==self.sensors,"incorrect sizing of inputs"
+        #print(">>>",inputs,self.A(inputs))
         self.p=self.A(inputs) #run preprocessing
-        if self.p>self.t: self.p=self.t #not exceed
-        if self.p<=0: self.p=1 #non zero
+
         self.t+=1
         self.step(self.t)
         #neural post processing
-        out=self.x[:,self.t+1] + self.post_process_bias #get summed outputs of step
-        self.out=out
+        out=self.x[:,self.t+1] 
+        #print(">>>",self.x[:,self.t+1])
+        self.out=self.B(out) #+ self.post_process_bias #get summed outputs of step
         #place through neural post processing
         #send to motors
 
--- Hardware/CircuitPython/braitenberg.py
+++ Hardware/CircuitPython/braitenberg.py
@@ -17,21 +17,30 @@ sensorB=AnalogIn(board.GP27)
 def getSensor(): #get an input array of sensor readngs
     a=sensorA.value / 65535 * sensorA.reference_voltage /4.8
     b=sensorB.value / 65535 * sensorB.reference_voltage /4.8
-    return np.array([a,b]) *100
+    return 1-np.array([a,b]) 
 #setup brain
-b=Brain(2,2,size=100)
-d=[]
-ps=[]
+b=Brain(2,2,size=200)
 
-for i in range(b.steps-2): #main loop to run
-    inputs=getSensor() #grab sensor information
-    b.run(inputs)
-    print(b.out)
-    d.append(b.out)
-    ps.append(b.p)
-    #move motors based off output pattern
-    bot.motorOn(3, "r" if b.out[0]<0 else "f", 100*abs(b.out[0]))
-    bot.motorOn(4, "r" if b.out[1]<0 else "f", 100*abs(b.out[1]))
-d=np.array(d)
-
+for k in range(100):
+    for i in range(b.steps-2): #main loop to run
+        inputs=getSensor() #grab sensor information
+        b.run(inputs)
+        #failsafe
+        if maths.isnan(b.out[0]):
+            b.out[0]=0.0
+            b.reset()
+        if maths.isnan(b.out[1]):
+            b.out[1]=0.0
+            b.reset()
+        print(inputs,b.p,b.out)
+        #move motors based off output pattern
+        try:
+            bot.motorOn(3, "f" if b.out[0]<0 else "r", 100*abs(b.out[0]))
+            bot.motorOn(4, "f" if b.out[1]<0 else "r", 100*abs(b.out[1]))
+            #pass
+        except:
+            bot.stop()
+    bot.stop()
+    b.clearOld()
+    
 bot.stop()
