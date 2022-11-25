eec49dcc9280efc0be0559db2f3b708b3911d3aa
shepai <dextershepherd@hotmail.co.uk>
2022-11-25 12:01:58 +0000

    delays and resets

--- Hardware/CircuitPython/breitenberg sensory nn.py
+++ Hardware/CircuitPython/breitenberg sensory nn.py
@@ -59,11 +59,11 @@ def getDist():
 def getSensor(): #get an input array of sensor readngs
     a=sensorA.value / 65535 * sensorA.reference_voltage /4.8
     b=sensorB.value / 65535 * sensorB.reference_voltage /4.8
-    return 1-np.array([a,b])
+    return 1-np.array([a-0.1,b])
 #create motors
 Enable_motor() #from motor hat control
 
-b=Brain(2,2,size=200)
+b=Brain(2,2,size=50)
 b.runP(4) #get started
 p=[1,4,6]
 dirA=0
@@ -89,10 +89,12 @@ for i in range(100):
         dirB=1
         Motor1_reverse()
         Motor2_forward()
-    if getDist()[0] < 10:
+    if getDist()[0] < 10: #stop if obstacle in front to avoid damage
         dirA=0
         dirB=0
         Motor_stop()
+    if i%50==0: #prevent overflow
+        b.reset()
     sleep(0.5)
     Motor_stop()
     file.write(str(inputs[0])+","+str(inputs[1])+","+str(dirA)+","+str(dirB)+","+str(b.out[0])+","+str(b.out[1])+","+str(b.p)+"\n")
