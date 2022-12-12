9349e8e732202d55e312fdcfee6d5157eb5731ee
shepai <dextershepherd@hotmail.co.uk>
2022-12-12 13:12:51 +0000

    form

--- Bio-inspired sensors/Stretch/readStretch.py
+++ Bio-inspired sensors/Stretch/readStretch.py
@@ -1,8 +1,10 @@
+#Micropython code for reading sensor values
+
 from machine import Pin, ADC, PWM
 from utime import sleep
 
 
-st = ADC(Pin(27))
+st = ADC(Pin(27)) #connect to GP27
 
 def get():
-    print(st.read_u16())
+    print(st.read_u16()) #output reading
