ca6761a49039bf4d7f98745d9bafda8bff71cd0c
shepai <dextershepherd@hotmail.co.uk>
2022-12-24 09:21:40 +0000

    show timer predictions

--- Simulation/OpenAIGym/gym evolve chaos.py
+++ Simulation/OpenAIGym/gym evolve chaos.py
@@ -7,6 +7,9 @@ import random
 import math as maths
 import cv2
 
+from timer_pred import predictor
+import time
+
 SIM='MountainCar-v0'
 
 def run(agent,num_trials):
@@ -15,17 +18,20 @@ def run(agent,num_trials):
     victory=False
     _ =0
     route=[]
+    award=[]
     while _<num_trials and not victory:
         action=agent.forward(observation)
         observation, reward, terminated, truncated, info = env.step(action) #step through with each action
         if terminated or truncated: #environment finished
-            print(">>",observation)
-            victory=True
+            #print(">>",observation)
+            observation, info = env.reset()
+            #victory=True
+        award.append(fitness(observation))
         _+=1
         route.append(env.render())
     f=fitness(observation)
     observation, info = env.reset()
-    return f*0.5 + ((num_trials-_)/num_trials)*0.5, route#take into account how quickly it did it
+    return f, route#take into account how quickly it did it #*0.5 + ((num_trials-_)/num_trials)*0.5
 
 #create network for sensory controller
 network=Network(2) #fdiffrent direction
@@ -56,9 +62,8 @@ class controller:
         self.chaotic=chaotic
     def forward(self,input):
         #run through sensory processing
-        outcome=self.sensoryNN.forward(input)
+        outcome=self.sensoryNN.forward(input*10)
         ind=np.argmax(outcome)
-        vectors=[[1,1],[1,0],[0,1],[-1,0],[0,-1],[1,-1],[-1,1],[-1,-1]]
         if self.chaotic:
             p_value=self.chaoticNN.values[ind] #select p value to control chaos
             #get chaotic generation
@@ -102,7 +107,11 @@ def fitness(obs):
     worst=[-1.2, -0.07]
     total=maths.sqrt((worst[0]-best[0])**2 + (worst[1]-best[1])**2)
     dist=maths.sqrt((obs[0]-best[0])**2 + (obs[1]-best[1])**2)
-    return dist/total
+    return (total-dist)/total
+"""def fitness(observation):
+    best=[0.6, 0.07]
+    worst=[-1.2, -0.07]
+    return ((observation[0]))/(best[0]+abs(worst[0]))"""
 def run_controller(chaos_):
     #create chaotic neuron pair
     chaos=Brain() 
@@ -114,23 +123,23 @@ def run_controller(chaos_):
     
     #population of genotypes
     genotype = np.zeros((c.shape_of_weights))
-    SIZE=100
-    population=np.random.normal(0,20,(SIZE,genotype.shape[0]))+np.random.normal(0,2,(SIZE,genotype.shape[0]))
+    SIZE=50
+    population=np.random.normal(0,5,(SIZE,genotype.shape[0]))+np.random.normal(0,5,(SIZE,genotype.shape[0]))
     #genetic algorithm
     Gen=200
-    fitness=[0]
+    fitness=[-500]
     bestPath=[]
-
+    p=predictor(time.time())
     for i in range(Gen):
         #print("Generation:",i,"with fitness",fitness[-1])
         t1=random.randint(0,SIZE-1)
         t2 = t1+1 if (t1<SIZE-1) else t1-1 #local population tournaments
         #trial 1
         c.set_gene(population[t1])
-        f1,p1=run(c,150)
+        f1,p1=run(c,200)
         #trial 2
         c.set_gene(population[t2])
-        f2,p2=run(c,150)
+        f2,p2=run(c,200)
         #selection
         if f1>f2:
             population[t2]=deepcopy(c.mutate_gene(population[t2]))
@@ -144,9 +153,12 @@ def run_controller(chaos_):
         elif f1>fitness[-1]:
             bestPath=deepcopy(p2)
         fitness.append(max(fitness+[f1,f2]))
-        print("Top:",fitness[-1])
-    for i,rend in enumerate(bestPath):
-        cv2.imwrite("C:/Users/dexte/OneDrive/Pictures/Saved Pictures/PhD chaos/AutoGen/",rend)
+        p.calc_iteration(time.time())
+        #print("Gen:",i,"Top:",fitness[-1])
+        #print("Time left:",p.time_left(Gen-i)/60,"minutes")
+    if fitness[-1]>0.8:
+        for i,rend in enumerate(bestPath):
+            cv2.imwrite("C:/Users/dexte/OneDrive/Pictures/Saved Pictures/PhD chaos/AutoGen/file_"+str(i)+".png",rend)
         """plt.imshow(rend)
         plt.title("Best")
         plt.pause(0.01)
@@ -154,4 +166,47 @@ def run_controller(chaos_):
             plt.show() #wait for user to watch"""
     return fitness.index(fitness[-1]),fitness[-1] #get first occurence of best fitness
 
-run_controller(False)
+trials=50
+f=np.zeros((2,trials))
+f1=np.zeros((2,trials))
+print("RUN...")
+p_=predictor(time.time())
+for i in range(trials):
+    ind,fit=run_controller(False)
+    f1[0][i]=ind
+    f1[1][i]=fit*100
+    p_.calc_iteration(time.time())
+    print(">>",f1[1][i],f1[0][i])
+    print("Time left:",p_.time_left((trials*2) -i)/60,"minutes")
+for i in range(trials):
+    ind,fit=run_controller(True)
+    f[0][i]=ind
+    f[1][i]=fit*100
+    p_.calc_iteration(time.time())
+    print(">>",f[1][i],f[0][i])
+    print("Time left:",p_.time_left((trials) - i)/60,"minutes")
+fig,ax = plt.subplots(1,2,figsize=(9, 4))
+print(f)
+print(f1)
+bplot1 = ax[0].boxplot(list([f[0],f1[0]]),
+                     vert=True,  # vertical box alignment
+                     patch_artist=True,labels=["Chaotic neuron","FNN"])  # will be used to label x-ticks
+bplot2 = ax[1].boxplot(list([f[1],f1[1]]),
+                     vert=True,  # vertical box alignment
+                     patch_artist=True,labels=["Chaotic neuron","FNN"])  # will be used to label x-ticks
+
+
+ax[0].set_title('Shortest generation of convergence')
+
+ax[0].yaxis.grid(True)
+#ax[0].set_xlabel('Simulation models')
+ax[0].set_ylabel('Generation')
+
+ax[1].set_title('Simulation results as fitnesses over trials')
+
+ax[1].yaxis.grid(True)
+#ax[1].set_xlabel('Simulation models')
+ax[1].set_ylabel('Fitness of trials')
+
+fig.show()
+plt.show()
